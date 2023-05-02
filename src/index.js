dcf57300614dc49b42d4e6ee7234f0a23ef221e8
Yannic Oliva <olivy1@bfh.ch>
2023-05-02 15:44:52 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+console.log("hello world")
+
+const fs = require('fs');
+const path = require('path');
+
+function readDir(dir) {
+  fs.readdir(dir, (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    files.forEach(file => {
+      const filePath = path.join(dir, file);
+      fs.stat(filePath, (err, stat) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        if (stat.isDirectory()) {
+          readDir(filePath);
+        } else if (path.extname(file) === '.html') {
+          console.log(file);
+        }
+      });
+    });
+  });
+}
+readDir('C:/VSC');
